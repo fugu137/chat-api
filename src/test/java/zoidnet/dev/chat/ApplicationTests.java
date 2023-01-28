660d13901965bbf585c60ec185da40678f0e6b57
Michael Duncan <fugu137@gmail.com>
2023-01-28 23:55:40 +1100

    Initial commit with core spring boot setup

---
+++ src/test/java/zoidnet/dev/chat/ApplicationTests.java
@@ -0,0 +1,13 @@
+package zoidnet.dev.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextShouldLoad() {
+	}
+}
