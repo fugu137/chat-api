fae18e1f686bf6a99a133f8bf54e5922e8367a10
Michael Duncan <fugu137@gmail.com>
2023-01-29 14:53:07 +1100

    Added first application test

--- src/test/java/zoidnet/dev/chat/ApplicationTests.java
+++ src/test/java/zoidnet/dev/chat/ApplicationTests.java
@@ -1,13 +1,21 @@
 package zoidnet.dev.chat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import zoidnet.dev.chat.repository.UserRepository;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
 	void contextShouldLoad() {
+		assertThat(userRepository).isNotNull();
 	}
 }
